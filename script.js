9bad0256cd0955e1af2210498edf2e6e770b1bbf
Дима Ш. <dima.o.sh@proton.me>
2025-08-13 19:46:30 +0100

    Initial commit
    * Added basic functionality for the website:
    	* Added basic webpage with a button.
    	* When the button is pressed, the clipboard will be spammed.

---
+++ script.js
@@ -0,0 +1,17 @@
+const characters = "abcdefghijklmnopqrstuvwxyz";
+
+async function spamClipboard() {
+	for (let i = 0; i < 10; i++) {
+		let str = "";
+
+		for (let j = 0; j < 10; j++) {
+			str += characters[Math.floor(Math.random() * characters.length)];
+		}
+
+		console.log("Copying string", str);
+		await navigator.clipboard.writeText(str);
+		await new Promise(res => setTimeout(res, 15));
+	}
+};
+
+exports = spamClipboard;
