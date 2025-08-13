f0fc2d4f31050f527aa57d4bed81d8c58be61d3a
Дима Ш. <dima.o.sh@proton.me>
2025-08-13 21:47:40 +0100

    Add simple logging system

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const characters = "abcdefghijklmnopqrstuvwxyz";
 
+const copied = [];
+
 async function spamClipboard() {
 	for (let i = 0; i < 10; i++) {
 		let str = "";
@@ -10,6 +12,14 @@ async function spamClipboard() {
 
 		console.log("Copying string", str);
 		await navigator.clipboard.writeText(str);
+		copied.push(str);
+		updateLogs();
 		await new Promise(res => setTimeout(res, 15));
 	}
 };
+
+function updateLogs() {
+	const p = document.querySelector("p.logs");
+
+	p.innerHTML = copied.map((entry) => `Copied text "${entry}"`).join("<br />");
+}
